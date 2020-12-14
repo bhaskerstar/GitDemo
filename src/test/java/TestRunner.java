2b127e554c5f9cf69359486164b592f0cd881183
bhasker <chaurasiaavika@gmail.com>
2020-12-13 20:17:25 -0500

    first code but its wrong

---
+++ src/test/java/TestRunner.java
@@ -0,0 +1,8 @@
+import org.junit.runner.RunWith;
+
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@Cucumber.Options(format={"pretty","html:reports/test-report"},tags= "@smokeTest")
+public class TestRunner {
+}
